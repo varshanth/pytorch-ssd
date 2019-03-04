a0ce00605a0d7bba913220f1a3668dbe6ba3222b
tuhintiwari <tuhintiwari25@gmail.com>
2019-03-04 14:52:36 -0500

    Converting ndarray to list

--- run_ssd_example.py
+++ run_ssd_example.py
@@ -70,7 +70,7 @@ for i in range(boxes.size(0)):
 
     if labels[i] == 15:
     	data['person'].append({
-		'boxes': box.cpu().numpy(),
+		'boxes': box.cpu().numpy().tolist(),
 		'distance': '250'
 	})
 
