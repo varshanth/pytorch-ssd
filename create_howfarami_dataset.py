a5488bb8451277b35af2eb57c65e9b5fc763b775
tuhintiwari <tuhintiwari25@gmail.com>
2019-03-10 20:03:39 -0400

    Rotating images if height is greater than width

--- create_howfarami_dataset.py
+++ create_howfarami_dataset.py
@@ -57,17 +57,23 @@ data = []
 for images in os.listdir(images_folder):
 	pil_image = Image.open(images_folder + "/" + images).convert('RGB')
 	orig_image = np.array(pil_image)
-	image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+	trans_image = orig_image
+    	#if height
+    	height, width, channels = trans_image.shape
+	# rotating the image if height is greater than width
+    	if height < width:
+        	trans_image = cv2.rotate(orig_image, cv2.ROTATE_90_CLOCKWISE)
+	image = cv2.cvtColor(trans_image, cv2.COLOR_BGR2RGB)
 	boxes, labels, probs = predictor.predict(image, 10, 0.4)
 
 	img_id = os.path.splitext(images)[0]
 	data_per_image = {'boxes' : [], 'labels' : [], 'distances' : [], 'image_id' : img_id}
 	for i in range(boxes.size(0)):
 		box = boxes[i, :]
-		cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
+		cv2.rectangle(trans_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
 		#label = f"""{voc_dataset.class_names[labels[i]]}: {probs[i]:.2f}"""
 		label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
-		cv2.putText(orig_image, label,
+		cv2.putText(trans_image, label,
 			(box[0] + 20, box[1] + 40),
 			cv2.FONT_HERSHEY_SIMPLEX,
 			1,  # font scale
@@ -84,5 +90,5 @@ for images in os.listdir(images_folder):
 with open('data.txt', 'w') as outfile:
         json.dump(data, outfile)
 path = "run_ssd_example_output.jpg"
-cv2.imwrite(path, orig_image)
+cv2.imwrite(path, trans_image)
 print(f"Found {len(probs)} objects. The output image is {path}")
