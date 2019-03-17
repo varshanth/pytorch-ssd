eac88feb1497556471fdbc5a72acd9716837bfda
Varshanth <varshanth.rao99@gmail.com>
2019-03-17 14:51:57 -0400

    Added data split code

---
+++ how_far_am_i/README.md
@@ -0,0 +1,5 @@
+# Instructions
+
+1) Copy all .jpg into images directory
+2) Copy labels file into "labels.json"
+3) Run train_test_split.py
---
+++ how_far_am_i/train_test_split.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+
+train_frac = 0.8
+ds_root = './'
+train_dir = f"{ds_root}/train"
+test_dir = f"{ds_root}/test"
+
+with open('labels.json', 'r') as f:
+    content = json.load(f)
+
+num_train = int(train_frac * len(content))
+
+train_content = content[:num_train]
+test_content = content[num_train:]
+
+with open('train_labels.json', 'w') as f:
+    json.dump(train_content, f)
+
+with open('test_labels.json', 'w') as f:
+    json.dump(test_content, f)
+
+
+for train_img_info in content[:num_train]:
+    img_name = f"{train_img_info['image_id']}.jpg"
+    os.rename(f"{ds_root}/images/{img_name}", f"{train_dir}/{img_name}")
+
+for test_img_info in content[num_train:]:
+    img_name = f"{test_img_info['image_id']}.jpg"
+    os.rename(f"{ds_root}/images/{img_name}", f"{test_dir}/{img_name}")
+
+
